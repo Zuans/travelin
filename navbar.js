dd7c3da1fe14c5ee5ed6c4349da85e3a49a6e11a
Zuans <juandev.net@gmail.com>
2024-12-07 10:49:15 +0700

    init file

---
+++ navbar.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('#jumbotron');
+  header.addEventListener('scroll', () => {
+    console.log('eeawtaweta')
+  })
+});
